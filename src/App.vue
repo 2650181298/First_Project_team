096f6b02287a566c57258bd51fc210946c3627b5
2650181298 <2650181298@qq.com>
2019-06-14 08:35:18 +0800

    o

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <div id="nav">
+      <router-link to="/">班级列表</router-link> |
+      <router-link to="/about">学生列表</router-link>
+    </div>
+      <router-view/>
+
+  </div>
+    
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home">
+        <p>hello vue</p>
+    </div>
+</template>
+
+
+
+<script>
+// @ is an alias to /src
+
+</script>
